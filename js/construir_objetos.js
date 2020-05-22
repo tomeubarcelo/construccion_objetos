0fb342cd482c20c9261772f80ad24fe7962bb635
Tomeu Barcelo <tomeu.95@gmail.com>
2020-05-22 15:43:30 +0200

    Mejoras creacion objetos

--- js/construir_objetos.js
+++ js/construir_objetos.js
@@ -1,4 +1,3 @@
-//CONSTRUCTORES
 //constructor del objeto coche con cuatro parametros
 function coche(marca, modelo, anyo, color) {
     this.marca = marca;
@@ -13,11 +12,14 @@ var carsModel = ['Civic', 'Ibiza', 'Yaris', 'i30', 'Corsa', 'R8', 'i8', 'Lancer'
 var carsYear = [2001, 2003, 2005, 2007, 2011, 2012, 2015, 2017, 2019, 2020];
 var carsColor = ['Negro', 'Rojo', 'Gris', 'Negro', 'Azul', 'Gris', 'Blanco', 'Azul', 'Negro', 'Rojo'];
 
-
+var arrayCars = []; //array para almacenar los objetos
 //con un bucle flor vamos instanciando nuevos objetos a traves del constructor
 for (let i = 0; i < carsName.length; i++) {
-    var objetcCar = new coche(carsName[i], carsModel[i], carsYear[i], carsColor[i]);
+    var car = new coche(carsName[i], carsModel[i], carsYear[i], carsColor[i]); //declaramos el nuevo coche
+    arrayCars.push(car); //añadimos el objeto coche al array
     var description = (i + 1) + "- " + carsName[i] + " " + carsModel[i] + " " + carsYear[i] + " " + carsColor[i];
     document.write(description + "<br>");
-    console.log(objetcCar);
+    console.log(car);
 }
+
+console.log(arrayCars);
